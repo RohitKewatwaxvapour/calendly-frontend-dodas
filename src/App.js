c86a101cc2e48f1263e47c08fff5f956b9c4fee8
RohitKewat <developer@waxvapour.com>
2023-08-23 18:19:01 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { PopupWidget, PopupButton, InlineWidget } from "react-calendly";
+import { Helmet } from 'react-helmet';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://assets.calendly.com/assets/external/widget.js';
+    script.async = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Dodas test 
+      <iframe  
+        src="https://calendly.com/dodoas/dodas-test"
+        width="70%"
+        height="600"
+        frameborder="0"
+      ></iframe>
     </div>
   );
 }
